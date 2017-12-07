53c4b1134a0b921ec6c296d0bf8d8a24d84d4c3d
vicmans <vsanchezl94@gmail.com>
2017-12-06 21:07:28 -0400

    Ahora lee el archivo que uno escriba

--- prueba1.f90
+++ prueba1.f90
@@ -2,9 +2,13 @@ program prueba1
  use mesh
  ! prueba leyendo un archivo mesh .msh
  TYPE(mesh_container) :: meshs
- meshs = load_mesh('sphere.msh')
+ CHARACTER(LEN=40) :: ar
+ print *, 'Introduzca el archivo .msh que quiere leer'
+ read *, ar
+
+ meshs = load_mesh(ar)
  print *, 'Ahora vamos a esportarlo'
  
  call export_mesh('nuevo.msh',meshs)
- print *, 'Fino, ojala aiga servido'
+ print *, 'Fino, ojala aiga servido, vea el archivo nuevo.msh'
 end program prueba1
