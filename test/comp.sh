ca65a6d40702dacb5c47127f8601993ede6cce7c
vicmans <vsanchezl94@gmail.com>
2018-01-01 14:51:46 -0400

    Compila la prueba2

--- test/comp.sh
+++ test/comp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "compilando"
-gfortran -o prueba2 -fcheck=all -Wall constants.f90 linalg.f90 mesh.f90 aux.f90 prueba2.f90
+gfortran -o prueba2 -fcheck=all -Wall constants.f90 linalg.f90 mesh.f90 aus.f90 prueba2.f90
 echo "Listo. Ejecute prueba2"
